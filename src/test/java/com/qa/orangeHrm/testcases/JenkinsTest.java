c6654085aac4bdab40ff42b7d21fbb7129890c37
QA_Lead <belliappa.bhavesh91@gmail.com>
2026-03-31 20:10:59 +0530

    Updated testclass for jenkins

--- src/test/java/com/qa/orangeHrm/testcases/JenkinsTest.java
+++ src/test/java/com/qa/orangeHrm/testcases/JenkinsTest.java
@@ -1,5 +1,6 @@
 package com.qa.orangeHrm.testcases;
 
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 import com.qa.orangeHrm.Base.BaseTest;
@@ -8,5 +9,6 @@ public class JenkinsTest extends BaseTest{
 	@Test
 	public static void main(String[] args)  {
 		System.out.println("Testing for NG_Testing and Jenkins");
+		Reporter.log("Testing for NG_Testing and Jenkins");
 	}
 }
